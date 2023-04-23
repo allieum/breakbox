93c5589f9e1d273594ddec87545a86a6a639b0cc
Rachel Robinson <rachel@toast>
2023-04-23 16:34:07 -0700

    time based

--- break.py
+++ break.py
@@ -16,6 +16,7 @@ for i in range(pygame.midi.get_count()):
         print("using device ", i)
 
 sound = pygame.mixer.Sound("143-2bar-000.wav")
+# sound = pygame.mixer.Sound("click143.wav")
 # while pygame.mixer.get_busy() == True:
 #     continue
 
@@ -24,18 +25,20 @@ START = 0b11111010
 STOP = 0b11111100
 
 # probably better to do this with milliseconds so it's agnostic to BPM
-CLOCK_LAG = 2
+# CLOCK_LAG = 2
 
 
 
-beat_interval = None
+beat_interval = 42069
 beat_start = time.time()
 
+lag_time = 0.028
 max_beats = 8
 beat = 0
 clock_count = 0
 midi = pygame.midi.Input(device_id)
-is_playing = False
+is_started = False
+played_sound = False
 while True:
     # time.sleep(1)
     events = midi.read(1)
@@ -43,15 +46,14 @@ while True:
         (status, d1, d2, d3) = events[0][0]
 
         if status == START:
-            is_playing = True
-            # head start to make up for lag
-            clock_count = CLOCK_LAG
+            is_started = True
+            clock_count = 0
             beat = 0
             # could play sample with later start time so it's not out of sync the first time around
             sound.play()
 
         if status == STOP:
-            is_playing = False
+            is_started = False
 
 
         if status == CLOCK:
@@ -64,8 +66,13 @@ while True:
             now = time.time()
             beat_interval = now - beat_start
             beat_start = now
+            if not played_sound and beat == 0 and is_started:
+                sound.play()
+            played_sound = False
 
         # if beat == 0 and clock_count == 0 and is_playing:
         #     sound.play()
-
-        if beat == max_beats - 1 and time.time() - beat_start:
+        beat_predicted = time.time() - beat_start >= beat_interval - lag_time
+        if beat == max_beats - 1 and beat_predicted and not played_sound and is_started:
+            sound.play()
+            played_sound = True
