a47da76494b9259907ba6d6e3d4f6a5c8776ac6d
Rachel Robinson <rachel@toast>
2023-07-03 20:07:04 -0700

    ugh

--- lights.py
+++ lights.py
@@ -78,7 +78,7 @@ class SampleState:
     playing: bool = field(default=False)
     bank: int = field(default=0)
     length: float = field(default=0, compare=False)
-    steps: int = field(default=0)
+    steps: int = field(default=0, compare=False)
     selected: bool = field(default=False)
     recording: bool = field(default=False)
     step: int | None = field(compare=False, default=None)
@@ -117,8 +117,8 @@ class LedState:
             fade_color, start, duration, strength = item
             if time.time() - start > duration:
                 self.fade_goals.remove(item)
-                self.mix(fade_color, strength)
-                self.mix(0x0, 0.08)
+            self.mix(fade_color, strength)
+        self.mix(0x0, 0.08)
 
     def write(self):
         logger.debug(f"{self.i} setting to {self.color}")
@@ -142,29 +142,29 @@ def init():
 #     # too_soon = time.time() - last_update < REFRESH_INTERVAL
 #     return samples_on != last_state
 
-def show_param(states: list[LedState], value, param: modulation.Param, color):
-    if param.max_value is None or param.min_value is None:
-        return
-    range = param.max_value - param.min_value
-    norm = value - param.min_value
-    ratio = norm / range
-    lights_value = 6 * ratio
-    entire_lights = math.floor(lights_value)
-    partial_light = lights_value - entire_lights
-    for led in states[:entire_lights]:
-        led.fade(color, 2, 0.6)
-        led.update()
-        led.leds.show()
-    if partial_light > 0:
-        led = states[entire_lights]
-        led.fade(color, 2, 0.6)
-        led.fade(OFF, 2, 0.6)
-        led.update()
-        led.leds.show()
+# def show_param(states: list[LedState], value, param: modulation.Param, color):
+#     if param.max_value is None or param.min_value is None:
+#         return
+#     range = param.max_value - param.min_value
+#     norm = value - param.min_value
+#     ratio = norm / range
+#     lights_value = 6 * ratio
+#     entire_lights = math.floor(lights_value)
+#     partial_light = lights_value - entire_lights
+#     for led in states[:entire_lights]:
+#         led.fade(color, 2, 0.6)
+#         led.update()
+#         led.leds.show()
+#     if partial_light > 0:
+#         led = states[entire_lights]
+#         led.fade(color, 2, 0.6)
+#         led.fade(OFF, 2, 0.6)
+#         led.update()
+#         led.leds.show()
 
-def register_param(states, param: modulation.Param, color):
-    param.on_change = lambda value: show_param(states, value, param, color)
-    # put a thing on sample state for param change notify
+# def register_param(states, param: modulation.Param, color):
+#     param.on_change = lambda value: show_param(states, value, param, color)
+#     # put a thing on sample state for param change notify
 
 def run(lights_q: Queue):
     # global last_update, last_state, refreshing
@@ -217,7 +217,7 @@ def run(lights_q: Queue):
                 led.write()
             leds.show()
             # logger.info(f"{selected_sample} vs ")
-            # logger.info(f"updating lights")
+            logger.info(f"updating lights")
         else:
             led_states = last_state
             sample_states = prev_samples
